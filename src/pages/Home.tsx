c1f1dbd7bdc7ce4d08ef2c07f98a0e19cf6fee29
alfonsoaranzazu <alfonso@viruett.com>
2022-12-10 01:28:20 -0800

    apply selected border color

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
-import { Avatar, IconButton } from '@mui/material';
+import { Avatar, IconButton, useTheme  } from '@mui/material';
 import { Stack } from '@mui/system';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function Home() {
+  const theme = useTheme();
   const baseUrl = 'https://froggyfriends.mypinata.cloud/ipfs';
   const { cid2d, cid3d, cidPixel } = useParams();
   const frog2dUrl = `${baseUrl}/${cid2d}`;
@@ -11,12 +12,22 @@ export default function Home() {
   const frogPixelUrl = `${baseUrl}/${cidPixel}`;
   const [type, setType] = useState<string>(frog2dUrl);
 
+  const getBorder = (url: string) => {
+    return url === type ? `3px solid ${theme.palette.primary.main}` : 'none'
+  }
+
   return (
     <Stack>
       <Stack direction='row' justifyContent='center'>
-        <IconButton onClick={() => setType(frog2dUrl)}><Avatar src={frog2dUrl}/></IconButton>
-        <IconButton onClick={() => setType(frog3dUrl)}><Avatar src={frog3dUrl}/></IconButton>
-        <IconButton onClick={() => setType(frogPixelUrl)}><Avatar src={frogPixelUrl}/></IconButton>
+        <IconButton size='large' onClick={() => setType(frog2dUrl)}>
+          <Avatar src={frog2dUrl} sx={{border: getBorder(frog2dUrl), height: 50, width: 50}}/>
+        </IconButton>
+        <IconButton onClick={() => setType(frog3dUrl)}>
+          <Avatar src={frog3dUrl} sx={{border: getBorder(frog3dUrl), height: 50, width: 50}}/>
+          </IconButton>
+        <IconButton onClick={() => setType(frogPixelUrl)}>
+          <Avatar src={frogPixelUrl} sx={{border: getBorder(frogPixelUrl), height: 50, width: 50}}/>
+        </IconButton>
       </Stack>
       <img src={type}/>
     </Stack>
